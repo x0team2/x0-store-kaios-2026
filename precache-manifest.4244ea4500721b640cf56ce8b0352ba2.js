fe1bfd90c7e73949d3e050afa9e3195d16b20163
x0team2 <x0team2@x0.rf.gd>
2026-08-07 15:49:34 +0900

    Update precache-manifest.4244ea4500721b640cf56ce8b0352ba2.js

---
+++ precache-manifest.4244ea4500721b640cf56ce8b0352ba2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "080d351ace3db9e7b8a4",
+    "url": "/static/css/main.bfcb2c6a.chunk.css"
+  },
+  {
+    "revision": "080d351ace3db9e7b8a4",
+    "url": "/static/js/main.080d351a.chunk.js"
+  },
+  {
+    "revision": "d53d57e405a7cf0b07ab",
+    "url": "/static/js/runtime~main.d53d57e4.js"
+  },
+  {
+    "revision": "5ec86cabbcbf64bd8222",
+    "url": "/static/css/2.3b9d492f.chunk.css"
+  },
+  {
+    "revision": "5ec86cabbcbf64bd8222",
+    "url": "/static/js/2.5ec86cab.chunk.js"
+  },
+  {
+    "revision": "686d74dfef715329736523da67a68ce0",
+    "url": "/static/media/kai-icons.686d74df.eot"
+  },
+  {
+    "revision": "5a1c5c18aca6765abadea46336312509",
+    "url": "/static/media/kai-icons.5a1c5c18.ttf"
+  },
+  {
+    "revision": "4e2f66190ab901acc056c5aa6d1cac58",
+    "url": "/static/media/kai-icons.4e2f6619.woff"
+  },
+  {
+    "revision": "07127065df308060d7243d0d914b0819",
+    "url": "/static/media/kai-icons.07127065.svg"
+  },
+  {
+    "revision": "926bdcfe4e3efbe8c086d270eab51463",
+    "url": "/index.html"
+  }
+];
